24505a63aa5569cff2f5da79affaa7b1d71ed0ed
Snowy <snowyyang@outlook.com>
2021-12-09 23:34:05 +0800

    product table

---
+++ App_Data/dbo.Product.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [CategoryId] INT NOT NULL, 
+    [Title] NVARCHAR(50) NOT NULL, 
+    [ImageUrl] VARCHAR(255) NOT NULL, 
+    [DetailHtml] NVARCHAR(MAX) NOT NULL, 
+    [Price] NUMERIC NOT NULL, 
+    [Inventory] INT NOT NULL
+)
