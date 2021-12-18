0ccfd64dc8de82ac63da69944af701ec72fd3bcd
Snowy <snowyyang@outlook.com>
2021-12-18 17:20:18 +0800

    update products structure

--- App_Data/Products.sql
+++ App_Data/Products.sql
@@ -1,10 +1,26 @@
-﻿CREATE TABLE [dbo].[Products]
+﻿USE [SimpleCart]
+GO
+
+/****** Object:  Table [dbo].[Products]    Script Date: 2021/12/18 17:19:43 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Products](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[CategoryId] [int] NOT NULL,
+	[Title] [nvarchar](50) NOT NULL,
+	[ImageUrl] [varchar](255) NOT NULL,
+	[DetailHtml] [nvarchar](max) NOT NULL,
+	[Price] [numeric](18, 0) NOT NULL,
+	[Inventory] [int] NOT NULL,
+ CONSTRAINT [PK__Products__3214EC0738CEE214] PRIMARY KEY CLUSTERED 
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [CategoryId] INT NOT NULL, 
-    [Title] NVARCHAR(50) NOT NULL, 
-    [ImageUrl] VARCHAR(255) NOT NULL, 
-    [DetailHtml] NVARCHAR(MAX) NOT NULL, 
-    [Price] NUMERIC NOT NULL, 
-    [Inventory] INT NOT NULL
-)
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
